9ea121ae40e7ebf6ec11406d1e007fffb1bbe8e6
Carlos David López Hinojosa <59139466+CarlosDavidLopezHinojosa@users.noreply.github.com>
2024-11-17 19:17:23 +0100

    Docu

---
+++ package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"business.DTOs"},{"l":"business.DTOs.Reservas"},{"l":"business.Gestores"},{"l":"data.common"},{"l":"data.DAOs"}];updateSearchResults();
